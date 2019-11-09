09acecfa8fc32663e005fc42864346e39c0b94e7
lukebmorrow <lukebmorrow@gmail.com>
2019-11-09 15:32:12 -0600

    Finished animations with flipping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5;
     public Rigidbody2D rb;
+    public Animator animator;
     Vector2 movement;
+    public SpriteRenderer spriteRenderer;
 
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 
 	// Update is called once per frame
@@ -18,11 +22,17 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("speed", movement.sqrMagnitude);
+        if (movement.x < 0 && !spriteRenderer.flipX || movement.x > 0 && spriteRenderer.flipX)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
     }
 }
