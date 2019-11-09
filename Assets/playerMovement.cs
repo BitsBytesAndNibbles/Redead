6eaae3c9e65ec21c88dfe28e232248ef13858091
lukebmorrow <lukebmorrow@gmail.com>
2019-11-09 14:26:25 -0600

    Commiting playerMovement.cs

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public boolean isright;
+
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if(movement.)
+        animator.SetFloat("speed",movement.sqrMagnitude);
+        
+    }
+
+    void FixedUpdate()
+    {
+        //movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    }
+}
