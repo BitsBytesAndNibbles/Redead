5f36526fd5d6906e15598a02f101159258180d07
TNGDev <nqcuong2@gmail.com>
2019-11-09 20:02:18 -0600

    Solve conflict

---
+++ Assets/Scripts/EnemyGFXController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFXController : MonoBehaviour
+{
+    private AIPath enemy;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponentInParent<AIPath>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(enemy.currentDir[0]));
+        if (enemy.currentDir[0] < 0 && !spriteRenderer.flipX || enemy.currentDir[0] > 0 && spriteRenderer.flipX)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (collision.tag == "Player")
+        {
+            animator.SetBool("attack", false);
+        }
+    }
+
+}
